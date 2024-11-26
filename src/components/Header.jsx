ce4021a7a658d512f6c4ab993abf90c47e3b4e40
itksweb <itksweb@gmail.com>
2024-11-26 16:12:33 +0100

    Tailwind css updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import { switchTheme } from "../store/calcSlice";
-import { useEffect } from "react";
 
 const Header = () => {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.calc.theme);
-
-  let switchPosition; // position of the switch button in the theme switcher
-
-  useEffect(() => {
-    switch (theme) {
-      case "1":
-        switchPosition = "justify-start";
-        break;
-      case "2":
-        switchPosition = "justify-center";
-        break;
-      case "3":
-        switchPosition = "justify-end";
-        break;
-      default:
-        break;
-    }
-  }, [theme]);
 
   return (
     <header className="flex justify-between items-center px-1 py-3 w-[345px] ">
@@ -36,7 +17,9 @@ const Header = () => {
             <span className="w-[14px] text-center">3</span>
           </div>
           <div
-            className={`theme-switcher w-[50px] h-[14px] border-none bg-inputs-display-bg rounded-[10px] flex items-center justify-center cursor-pointer`}
+            className={`theme-switcher w-[50px] h-[14px] border-none cursor-pointer bg-inputs-display-bg rounded-[10px] flex items-center justify-${
+              theme === 1 ? "start" : theme === 2 ? "center" : "end"
+            } `}
             onClick={() => dispatch(switchTheme())}
           >
             <span className="theme-switcher-inner border-none rounded-full size-3 bg-primary-color "></span>
